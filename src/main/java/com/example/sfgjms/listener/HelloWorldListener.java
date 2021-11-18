9b65413531c3ddf25820f5fb8708078257c4b9cb
vitaliikavun <kvital@amazon.com>
2021-11-18 15:29:44 +0100

    added message sender and receiver via activemq server

---
+++ src/main/java/com/example/sfgjms/listener/HelloWorldListener.java
@@ -0,0 +1,23 @@
+package com.example.sfgjms.listener;
+
+import com.example.sfgjms.config.JmsConfig;
+import com.example.sfgjms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+@Component
+public class HelloWorldListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                        @Headers MessageHeaders headers, Message message) {
+        System.out.println("I Got message");
+        System.out.println(helloWorldMessage);
+    }
+
+}
---
+++ src/main/java/com/example/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package com.example.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -243557671545361L;
+
+    private UUID id;
+    private String message;
+}
